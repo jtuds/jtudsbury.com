4b16d8508527716b3be76378414e62741822ed38
jtuds <james@candidsky.com>
2016-03-14 11:28:44 +0000

    Responsive stuff

--- js/main.js
+++ js/main.js
@@ -4,5 +4,21 @@ $(document).ready(function(){
     slidesToShow: 3,
     slidesToScroll: 3,
     lazyLoad: 'progressive',
+    responsive: [
+	    {
+	    	breakpoint: 960,
+	    	settings: {
+	    		slidesToShow: 2,
+	    		slidesToScroll: 2
+	      	}
+	    },
+	    {
+	    	breakpoint: 620,
+	    	settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+	      	}
+	    }
+    ]
   });
 });
