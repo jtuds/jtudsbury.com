b4f85b77cd1e837e3665d6563c22d57deaa1ce85
Your Name <you@email.com>
2016-05-28 23:18:22 +0100

    Added a test post. Setup FTPLOY on the server

--- _site/js/single.js
+++ _site/js/single.js
@@ -1,1 +1,38 @@
-var getGif=function(){var a=[];return $("img").each(function(){var b=$(this).data("gif");a.push(b)}),a},gif=getGif(),defaultButtonText=$(".portfolio__figure__gif-trigger").text();$(".portfolio__figure__gif-trigger").on("click",function(){"Stop GIF"==$(".portfolio__figure__gif-trigger").text()?(console.log("true"),$(".portfolio__figure__gif-trigger").text(defaultButtonText)):(console.log("false"),$(".portfolio__figure__gif-trigger").text("Stop GIF"));var a=$(this),b=(a.index(),a.siblings("img")),c=b.attr("src"),d=b.attr("data-gif"),e=d.split(".");"gif"===e[1]?b.attr("src",b.data("gif")).attr("data-gif",c):b.attr("src",d).attr("data-gif",b.data("gif"))});
+var getGif = function() {
+    var gif = [];
+    $('img').each(function() {
+        var data = $(this).data('gif');
+        gif.push(data);
+    });
+    return gif;
+}
+var gif = getGif();
+
+var defaultButtonText = $('.portfolio__figure__gif-trigger').text();
+
+$('.portfolio__figure__gif-trigger').on('click', function() {
+
+  if ($('.portfolio__figure__gif-trigger').text() == "Stop GIF") {
+    console.log("true");
+    $('.portfolio__figure__gif-trigger').text(defaultButtonText);
+  }
+  else {
+    console.log("false");
+    $('.portfolio__figure__gif-trigger').text("Stop GIF");
+  }
+ 
+  var $this   = $(this),
+          $index  = $this.index(),
+           
+          $img    = $this.siblings('img'),
+          $imgSrc = $img.attr('src'),
+          $imgAlt = $img.attr('data-gif'),
+          $imgExt = $imgAlt.split('.');
+           
+  if($imgExt[1] === 'gif') {
+      $img.attr('src', $img.data('gif')).attr('data-gif', $imgSrc);
+  } else {
+      $img.attr('src', $imgAlt).attr('data-gif', $img.data('gif'));
+  }
+ 
+});
